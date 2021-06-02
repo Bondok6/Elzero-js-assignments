a6869b48fd59273641d95388de258b8c543e43b0
Bondok6 <kirolousbondok6@gmail.com>
2021-06-03 01:51:32 +0200

    add challenges

---
+++ 010__017/assignment 01/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const numberOne = 10;
+const numberTwo = 20;
+
+console.log('' + numberOne + numberTwo);
+console.log(typeof ('' + numberOne + numberTwo));
+console.log(`${'' + numberOne + numberTwo}`);
+console.log(`${typeof ('' + numberOne + numberTwo)}`);
+
+console.log(numberOne + '\n' + numberTwo);
+console.log(`${numberOne}\n${numberTwo}`);
